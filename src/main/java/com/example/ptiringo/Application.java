cd0862b7e5ecdc496eaf09631ddbac9a1e1ee323
ptiringo <ptiringo@gmail.com>
2016-07-10 14:49:28 +0900

    Rename main class

--- src/main/java/com/example/ptiringo/Application.java
+++ src/main/java/com/example/ptiringo/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JapanCommercialMapApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JapanCommercialMapApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/test/java/com/example/ptiringo/ApplicationTests.java
+++ src/test/java/com/example/ptiringo/ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = JapanCommercialMapApiApplication.class)
-public class JapanCommercialMapApiApplicationTests {
+@SpringApplicationConfiguration(classes = Application.class)
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
