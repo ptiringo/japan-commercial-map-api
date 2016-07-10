f0b7b7a642e9bf76843d555b6f38bd8cf3715dc1
ptiringo <ptiringo@gmail.com>
2016-07-10 14:37:55 +0900

    Initial commit

---
+++ src/main/java/com/example/ptiringo/JapanCommercialMapApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.ptiringo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanCommercialMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapanCommercialMapApiApplication.class, args);
+	}
+}
