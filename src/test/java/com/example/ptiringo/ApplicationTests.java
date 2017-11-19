38811b2c0e672c854ed1ff371a50f0fe8087697b
ptiringo <ptiringo@gmail.com>
2017-11-19 21:30:45 +0900

    ユニットテストがエラーになっているのを修正 #17

--- src/test/java/com/example/ptiringo/ApplicationTests.java
+++ src/test/java/com/example/ptiringo/ApplicationTests.java
@@ -1,16 +1,26 @@
 package com.example.ptiringo;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.configuration.DockerComposeFiles;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+	@ClassRule
+	public static DockerComposeRule docker = DockerComposeRule.builder()
+			.file("docker-compose.yml").build();
+
 	@Test
 	public void contextLoads() {
+		Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("config.properties");
 	}
 
 }
