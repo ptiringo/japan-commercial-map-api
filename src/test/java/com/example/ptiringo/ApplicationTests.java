336e61bc4b97e128c16aa2dbb89cc4da45c125ee
ptiringo <ptiringo@gmail.com>
2017-11-20 23:52:36 +0900

    Organize import

--- src/test/java/com/example/ptiringo/ApplicationTests.java
+++ src/test/java/com/example/ptiringo/ApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.palantir.docker.compose.DockerComposeRule;
-import com.palantir.docker.compose.configuration.DockerComposeFiles;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
